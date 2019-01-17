1e632e48c3143d5e1306df833d575b0a2209cb09
rinsuki <428rinsuki+git@gmail.com>
2019-01-17 16:58:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# video-ss
+
+タブのスクショを撮ってvideoタグのところを切り抜くやつ。
+主にZenzaWatch向け
+
+## 使い方
+
+1. git cloneなりDownload ZIP→解凍なりで落としてくる
+1. manifest.jsonがあるところを chrome://extensions → パッケージ化されてない拡張機能を読み込む(ないときは右上のデベロッパーモードをONにする) から読み込む
+1. 適当に動画を開く
+1. 右上のVを押す
+1. スクショが撮れる
+1. うれしい
+
+## FAQ
+
+|Q|A|
+|---|---|
+|Chrome Web Storeに出せ|そのうち|
---
+++ popup.ts
@@ -0,0 +1,32 @@
+
+chrome.tabs.getSelected(tab => {
+    chrome.tabs.captureVisibleTab(tab.windowId, {format: "png"}, dataUrl => {
+        const img = new Image()
+        img.src = dataUrl
+        img.onload = () => {
+            const instantCanvas = document.createElement("canvas")
+            instantCanvas.width = img.width
+            instantCanvas.height = img.height
+            const instantCtx = instantCanvas.getContext("2d")!
+            instantCtx.drawImage(img, 0, 0)
+            chrome.tabs.executeScript({
+                code: `[window.devicePixelRatio, Array.from(document.getElementsByTagName("video")).map(v => JSON.parse(JSON.stringify(v.getBoundingClientRect())))]`
+            }, ([[dpi, videos, ...others]]) => {
+                console.log(dpi, videos, others)
+                videos.forEach((video: any) => {
+                    if (video.width === 0) return
+                    if (video.height === 0) return
+                    const canvas = document.createElement("canvas")
+                    canvas.width = video.width * dpi
+                    canvas.height = video.height * dpi
+                    const ctx = canvas.getContext("2d")!
+                    ctx.putImageData(instantCtx.getImageData(video.x * dpi, video.y * dpi, canvas.width, canvas.height), 0, 0)
+                    const img = new Image()
+                    img.src = canvas.toDataURL("image/png")
+                    img.width = 480
+                    document.getElementById("imgs")!.appendChild(img)
+                })
+            })
+        }
+    })
+})
